7545eb6e27e74ca411da8eb4212b19d528aeb941
Andressa.licio <andressa_licio@hotmail.com>
2020-07-17 16:12:02 +0100

    Uploading gitignore

---
+++ client/build/precache-manifest.147b5296d6116539c3e7f3b650422e5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6055c82a30af3b94803a569db972764e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0c56ca1e869221a3022",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "6716faa923562f9e54b6",
+    "url": "/static/css/main.93920bca.chunk.css"
+  },
+  {
+    "revision": "c0c56ca1e869221a3022",
+    "url": "/static/js/2.5175b8a2.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.5175b8a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6716faa923562f9e54b6",
+    "url": "/static/js/main.d29addbd.chunk.js"
+  },
+  {
+    "revision": "a600b51e8e64703e0b0d",
+    "url": "/static/js/runtime-main.945786e1.js"
+  }
+]);
